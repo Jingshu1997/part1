27795f61c91bb65bda0339e925a3e0ef5383410b
ZHANG_Jingshu <zhangjingshu1010@gmail.com>
2022-05-23 22:41:26 +0200

    unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,84 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-
-
-  const Header = (props) => {
-    return (
-      <h1>
-          Name of the course :{props.name}
-        
-      </h1>
-    )
-  }
-
-  const Content = (props) => {
-    return (
-      <div>
-        <p>
-       Part1:  {course.parts[0].name}, exercises: {course.parts[0].exercises}
-        </p>
-        <p>
-       Part2:  {course.parts[1].name}, exercises: {course.parts[1].exercises}
-        </p>
-        <p>
-       Part3: {course.parts[2].name}, exercises: {course.parts[2].exercises}
-        </p>
-      
-      
-      </div>
-    )
-  }
-  const Total = (props) => {
-
-  
-    const sum =course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises
-    return (
-      <div>
-        Number of exercises: 
-        <p>
-        {sum}
-        </p>
-
-      </div>
-    )
-  }
-
-
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-
     <div>
-      <Header course={course} />
-      <Content  part1={course.parts[0]} part2={course.parts[1]}part3={course.parts[2]}/>
-      
-      <Total  sum={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}/>
- 
+      code here
     </div>
-   
   )
 }
-
-
-
 
 export default App
