4c78b73e26b460c433e0134175dda8311f94b0f2
ZHANG_Jingshu <zhangjingshu1010@gmail.com>
2022-05-23 23:19:06 +0200

    test22

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,16 +1,47 @@
 import React, { useState } from 'react'
 
+const Good = props => <div>{props.good}</div>
+const Neutral = props => <div>{props.neutral}</div>
+const Bad = props => <div>{props.bad}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+
+
 const App = () => {
-  // save clicks of each button to own state
+
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+
+  const setToGood= newGood=> {setGood(newGood) }
+  const setToNeutral = newNeutral => {setNeutral(newNeutral) }
+  const setToBad = newBad => {setBad(newBad)}
+
+
   return (
+    
     <div>
-      code here
+  
+<h1>give feedback</h1>
+      <Button handleClick={() => setToGood(good + 1)} text="Good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setToBad(bad+ 1)} text="Bad" />
+    <h1>statistics</h1>
+      
+      
+     <p>good <Good good={good} /></p> 
+     <p>neutral <Neutral neutral={neutral} /></p> 
+     <p>bad <Bad bad={bad} /></p> 
+ 
+    
     </div>
   )
 }
-
 export default App
